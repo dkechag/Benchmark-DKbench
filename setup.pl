354801a847057e03ed26f5c89c149cb13d03261b
Dimitrios Kechagias <dimitrios.kechagias@spareroom.co.uk>
2021-12-05 01:06:40 +0000

    Setup script

--- setup.pl
+++ setup.pl
@@ -18,10 +18,10 @@ system "gunzip data/*.gz";
 system "cd data && tar xvf t.tar && rm t.tar";
 
 print "Installing reference cpan verions (with --force)...\n";
-# system "cpanm -f http://cpan.metacpan.org/authors/id/O/OA/OALDERS/HTML-Parser-3.76.tar.gz";
-# system "cpanm -f http://cpan.metacpan.org/authors/id/K/KE/KENTNL/HTML-Tree-5.07.tar.gz";
-# system "cpanm -f http://cpan.metacpan.org/authors/id/N/NI/NIGELM/HTML-Formatter-2.16.tar.gz";
-# system "cpanm -f http://cpan.metacpan.org/authors/id/K/KA/KAMELKEV/CSS-Inliner-4014.tar.gz";
-# system "cpanm -f http://cpan.metacpan.org/authors/id/C/CJ/CJFIELDS/BioPerl-1.7.8.tar.gz";
-# system "cpanm -f https://cpan.metacpan.org/authors/id/E/ET/ETHER/Moose-2.2201.tar.gz";
-# system "cpanm Astro::Coord::Constellations Test::Harness";
+system "cpanm -f http://cpan.metacpan.org/authors/id/O/OA/OALDERS/HTML-Parser-3.76.tar.gz";
+system "cpanm -f http://cpan.metacpan.org/authors/id/K/KE/KENTNL/HTML-Tree-5.07.tar.gz";
+system "cpanm -f http://cpan.metacpan.org/authors/id/N/NI/NIGELM/HTML-Formatter-2.16.tar.gz";
+system "cpanm -f http://cpan.metacpan.org/authors/id/K/KA/KAMELKEV/CSS-Inliner-4014.tar.gz";
+system "cpanm -f http://cpan.metacpan.org/authors/id/C/CJ/CJFIELDS/BioPerl-1.7.8.tar.gz";
+system "cpanm -f https://cpan.metacpan.org/authors/id/E/ET/ETHER/Moose-2.2201.tar.gz";
+system "cpanm Astro::Coord::Constellations Test::Harness";
